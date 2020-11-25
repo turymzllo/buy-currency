b99deacac41fa9b9c716c5b84af97cb9a603f76c
turymzllo <turymzllo@gmail.com>
2020-11-25 19:23:21 -0300

    Add Models
    - Currency.cs
    - Purchase.cs

---
+++ CurrencyExchange/src/CurrencyExchange.Core/Models/Currency.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CurrencyExchange.Core.Models
+{
+    public class Currency
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ISOCode { get; set; }
+        public decimal PurchaseLimit { get; set; }
+        public string ExchangeRateProvider { get; set; }
+        public bool UseUSDFactor { get; set; }
+        public decimal USDFactor { get; set; }
+    }
+}
---
+++ CurrencyExchange/src/CurrencyExchange.Core/Models/Purchase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CurrencyExchange.Core.Models
+{
+    public class Purchase
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public decimal AmountLC { get; set; }
+        public int CurrencyId { get; set; }
+        public decimal PurchaseAmount { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
